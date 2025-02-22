c3aed9bbd5553b0512c8c6ca7c0e0e8e27478ca7
Gustavo Amezcua <gusta2897@gmail.com>
2025-02-22 11:44:34 -0800

    First push

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,11 @@
+import NavBar from "../NavBar/NavBar";
+
+const HeroSection = () => {
+  return (
+    <section id="hero-section">
+      <NavBar />
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
